fd4134d8bc17dab20bb7abb4c952017c1ef3c4ae
Dongjae Shin <djshin0530@gmail.com>
2017-07-14 00:19:20 +0900

    move 1.TD_run+time from local directory

---
+++ 1.TD_run+time/m_funcs.py
@@ -0,0 +1,28 @@
+import numpy as np
+# Defining analytical solution------------------------------------------
+def GAUSS(X, T):
+	value = np.exp(-20.*(X-0.5)**2/(1.+80*T))/np.sqrt(1+80*T)
+	return value
+def EXACT(X, T):
+	value = GAUSS(X, T)-GAUSS(X-1., T)-GAUSS(X+1., T)
+	return value
+# Description of oscillations of the initial wavepacket-----------------
+def GAUSS_packet(X,K0,X0,SIGMA,DT):
+# ----------------------------------------------------------------------
+	if DT >= 1/K0**2: # -sqrt(1/DT) < K0 < sqrt(1/DT)
+		print 'error) DT < 1 / K0**2 should be satisfied.'
+		print 'error) K0 should be b/w -{0} and {1}'\
+			  .format(np.sqrt(1./DT), np.sqrt(1./DT))
+		exit()
+
+	value = np.exp(1j*K0*X) * np.exp(-(X-X0)**2/(2*SIGMA**2))
+	return value
+
+# Potential function----------------------------------------------------
+def POTENTIAL_SQW(X,X0,W,V0):
+# ----------------------------------------------------------------------
+	if X < X0-W/2 or X >= X0+W/2:
+		value = 0. 
+	elif X >= X0-W/2 and X < X0+W/2:
+		value = V0
+	return value
---
+++ 1.TD_run+time/m_propagator.py
@@ -0,0 +1,67 @@
+import numpy as np
+import m_linalg as linalg
+
+## This module contains several time propagator functions ---------------------
+## 1. Explicit method
+## 2. Crank-Nicolson method
+## Input:
+## Output: 
+## 170617 Dongjae Shin, EEWS, KAIST
+## ----------------------------------------------------------------------------
+
+## Time evolution of a function using explicit method (not used)------
+def update_explicit(i, PHI, NSTEP, DT, DTH):
+## ---------------------------------------------------------------------
+	# Loop over time steps using explicit method
+	POLD = 0.
+	for IX in range(1,NSTEP):
+		PNEW = PHI[IX]+DTH*(POLD+PHI[IX+1]-2*PHI[IX])
+		POLD = PHI[IX]	# roll POLD value
+		PHI[IX] = PNEW	# store new value
+	return PHI
+
+## Time evolution of wavefunction using Crank-Nicolson method ----------
+def update_CN(i, PHI, V, NSTEP, DT, H):
+## ---------------------------------------------------------------------
+	## Loop over time steps
+	## Initialization of imtermediate function(CHI), [A], {b}
+	A 			= np.zeros((NSTEP-1, NSTEP-1), dtype=complex)
+	b 			= np.zeros(NSTEP-1, dtype=complex)
+	CHI 		= np.zeros(NSTEP-1, dtype=complex)
+	CHI_0 		= 0. + 0j # boundary condition
+	CHI_NSTEP 	= 0. + 0j
+
+	## Construction of tridiagonal matrix [A] and vector {b}
+	for j in range(0,NSTEP-1): # loop from 1 to NSTEP-1
+		if j == 0:
+			A[j,j] 		= -2. + 2*1j*H**2/DT - H**2*V[j+1]
+			A[j,j+1] 	=  1.
+			b[j] 		= 4*1j*H**2/DT * PHI[j+1] - CHI_0
+		elif j == NSTEP-2:
+			A[j,j-1] 	= 1.
+			A[j,j] 		= -2. + 2*1j*H**2/DT - H**2*V[j+1]
+			b[j] 		= 4*1j*H**2/DT * PHI[j+1] - CHI_NSTEP
+		else:
+			A[j,j-1] 	= 1.
+			A[j,j] 		= -2. + 2*1j*H**2/DT - H**2*V[j+1]
+			A[j,j+1] 	= 1.
+			b[j] 		= 4*1j*H**2/DT * PHI[j+1]
+
+	CHI = linalg.gaussSeidel(A,b,0.001) 
+
+	## Update the function using intermediate function, CHI
+	for IX in range(1,NSTEP):
+		PHI[IX] = CHI[IX-1] - PHI[IX]
+
+    ## Normalization on each time step
+	NORM 	 = 0.
+	SQRTNORM = 0.
+	for IX in range(0,NSTEP): # integration of probability (trapezoidal)
+		NORM += np.absolute(PHI[IX])**2 + np.absolute(PHI[IX+1])**2
+	NORM *= H/2.0
+	SQRTNORM = np.sqrt(NORM)
+	for IX in range(1,NSTEP): # normalize the function
+		PHI[IX] /= SQRTNORM
+
+	return PHI
+
